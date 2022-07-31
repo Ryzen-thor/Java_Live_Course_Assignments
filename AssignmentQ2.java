6056399522ba1c20ce684975ae0a5d827253dbcb
Aman tiwari <aman.friend2016@gmail.com>
2022-07-31 23:26:45 +0530

    Update AssignmentQ2.java

--- AssignmentQ2.java
+++ AssignmentQ2.java
@@ -3,14 +3,11 @@ public class AssignmentQ2 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int row=10;
+		int row=5,col=5;
+		
 		for(int i=0;i<row;i++) {
-			for(int j=0;j<row;j++) {
-				if(i>0 && j>0 && i<row-1 && j<row-1 ) {
-					System.out.print(" ");
-				}else {
-					System.out.print("*");
-				}
+			for(int j=0;j<col;j++) {
+				System.out.print(i);
 			}
 			System.out.println();
 		}
